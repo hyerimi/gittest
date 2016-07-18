d7e00d78d000e8b4f09f5e5c81ee1e53d897de7c
hyerimi <01098805069@nate.com>
2016-07-18 12:42:23 +0900

    ...

--- day14/2.js
+++ day14/2.js
@@ -1,3 +1,12 @@
 /**
  * Created by KICT-13 on 2016-07-15.
  */
+const http =  require('http')
+const server = http.createServer(function (req,res) {
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/html");
+    res.end("<h1> hello world choi hye rim </h1>");
+});
+server.listen(8081,function() {
+    console.log("http server start")
+});
