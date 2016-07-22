956d8eb05090a858a93889e9c161b5212609e541
hyerimi <01098805069@nate.com>
2016-07-22 15:23:20 +0900

    ...

--- invader/game.c
+++ invader/game.c
@@ -16,69 +16,8 @@
 #include "alien.h"
 #include "game.h"
 
-/*
-struct timespec work_timer;
-double acc_tick, last_tick, fire_tick;
-int bLoop = 1;
-
-_S_MAP_OBJECT gScreenBuf[3];
-
-_S_MAP_OBJECT gPlayerModel;
-_S_MAP_OBJECT gBulletModel;
-_S_MAP_OBJECT gAlienModel;
-
-_S_Plane gPlayerObject;
-_S_BULLET_OBJECT gBulletObjects[6];
-_S_ALIEN_OBJECT gAlienObjects[6];
-*/
-
 int main()
 {
-
-
-
-	/*for(int i=0; i<2; i++)
-	{
-		map_init(&gScreenBuf[i]);
-		map_new(&gScreenBuf[i],40,24);
-	}
-
-	map_init(&gScreenBuf[2]);
-    map_load(&gScreenBuf[2],"ending.dat");
-
-	map_init(&gPlayerModel);
-	map_load(&gPlayerModel,"plane1.dat");
-
-	map_init(&gBulletModel);
-	map_load(&gBulletModel,"bullet.dat");
-
-	map_init(&gAlienModel);
-	map_load(&gAlienModel,"alien.dat");
-
-system("clear");
-
-	double TablePosition[] = {0,5,10,15,20,25};
-
-	for(int i=0; i<6; i++)
-	{
-		_S_ALIEN_OBJECT *pObj = &gAlienObjects[i];
-		alien_init(pObj,&gAlienModel);
-		pObj->m_fXpos =TablePosition[i]; 
-		pObj->m_fYpos = 2;
-		pObj->m_nFSM = 1;
-	} 
-	Plane_init(&gPlayerObject,&gPlayerModel,20,20);	
-	for(int i=0; i<sizeof(gBulletObjects)/sizeof(_S_BULLET_OBJECT); i++)
-	{
-		bullet_init(&gBulletObjects[i],0,0,0,&gBulletModel);
-	}
-
-	system("clear");
-
-	set_conio_terminal_mode();
-	acc_tick=last_tick=0;
-*/
-
 	while(bLoop) {
 	    if(nFSM == 0)
 	    {
